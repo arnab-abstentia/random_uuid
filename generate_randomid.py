343a773d52001a2aa6938065209cabf26ee82e4b
arnab-abstentia <50820739+arnab-abstentia@users.noreply.github.com>
2019-05-20 15:18:25 +0530

    Fixed a bug in number of row

--- generate_randomid.py
+++ generate_randomid.py
@@ -5,17 +5,18 @@ from uuid import uuid4
 TOTAL_UUID = 20_000_000
 BATCH_SIZE = 20_000_000
 NUM_BATCHES = TOTAL_UUID // BATCH_SIZE if TOTAL_UUID % BATCH_SIZE == 0 else TOTAL_UUID // BATCH_SIZE + 1
+NUM_ROWS = 20
 
 # running batch for every 20 million
 for i in range(NUM_BATCHES):
     # gurantees uniqueness for every 20 million uuid
     uid_container = [str(uuid4()).replace('-', '')[:8] for i in range(BATCH_SIZE)]
-    # creating batch of 10 rows
-    container_batch = [uid_container[x:x+20] for x in range(len(uid_container))]
+    # creating batch of 20 rows
+    container_batch = [uid_container[x:x+NUM_ROWS] for x in range(0, len(uid_container), NUM_ROWS)]
     # reducing memory footprint by deleting the set object
     del uid_container
     # writing the csv file
-    with open('random_uuid_sample.csv', 'a') as fd:
+    with open('random_uuid.csv', 'a') as fd:
         writer = csv.writer(fd)
         while len(container_batch) > 0:
             batch = container_batch.pop()
