7c47bd67892075b1267ecaf802a7490392a005a3
arnab-abstentia <50820739+arnab-abstentia@users.noreply.github.com>
2019-05-20 16:14:28 +0530

    updated the parameter for the func

--- generate_randomid.py
+++ generate_randomid.py
@@ -20,7 +20,7 @@ NUM_ROWS = 20
 for i in range(NUM_BATCHES):
     # gurantees uniqueness for every 20 million uuid
     # uid_container = [str(uuid4()).replace('-', '')[:8] for i in range(BATCH_SIZE)]
-    uid_container = list(generate_radom_uid())[:TOTAL_UUID]
+    uid_container = list(generate_radom_uid(num_uuid=TOTAL_UUID))[:TOTAL_UUID]
     # creating batch of 20 rows
     container_batch = [uid_container[x:x+NUM_ROWS] for x in range(0, len(uid_container), NUM_ROWS)]
     # reducing memory footprint by deleting the set object
