31f5121727be2115f1e54bf3f210b537f5a43ef7
arnab-abstentia <50820739+arnab-abstentia@users.noreply.github.com>
2019-05-20 14:46:32 +0530

    updated and removed function

--- generate_randomid.py
+++ generate_randomid.py
@@ -1,24 +1,21 @@
 import csv
-import uuid
 import sys
+from uuid import uuid4
 
-# function for generating  2 million uids
-def make_random_uuid(num_uids=2_000_000):
-    for _ in range(num_uids):
-        uid = uuid.uuid4()
-        yield str(uid).replace('-', '')
+TOTAL_UUID = 20_000_000
+BATCH_SIZE = 20_000_000
+NUM_BATCHES = TOTAL_UUID // BATCH_SIZE if TOTAL_UUID % BATCH_SIZE == 0 else NUM_BATCHES + 1
 
-# running batch for every 2 million
-for i in range(10):
-    # gurantees uniqueness for every 2 million uuid
-    uid_container = set(i for i in make_random_uuid())
-    uid_container = list(uid_container)
+# running batch for every 20 million
+for i in range(NUM_BATCHES):
+    # gurantees uniqueness for every 20 million uuid
+    uid_container = [str(uuid4()).replace('-', '')[:8] for i in range(BATCH_SIZE)]
     # creating batch of 10 rows
-    container_batch = [uid_container[x:x+10] for x in range(len(uid_container))]
+    container_batch = [uid_container[x:x+20] for x in range(len(uid_container))]
     # reducing memory footprint by deleting the set object
     del uid_container
     # writing the csv file
-    with open('random_uuid.csv', 'a') as fd:
+    with open('random_uuid_sample.csv', 'a') as fd:
         writer = csv.writer(fd)
         while len(container_batch) > 0:
             batch = container_batch.pop()
